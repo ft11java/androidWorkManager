981bdd5b539f95ed6bc4c31bb3de7890326bc015
ft11java <ft11java>
2023-11-27 15:00:47 +0300

    first commit

---
+++ app/src/main/java/com/taner/javaworkmanager/MainActivity.java
@@ -0,0 +1,79 @@
+package com.taner.javaworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Data data = new Data.Builder().putInt("intKey", 1).build();
+
+        Constraints constraints = new Constraints.Builder()
+                //.setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresCharging(false)
+                .build();
+
+       /*
+        WorkRequest workRequest=new OneTimeWorkRequest.Builder(RefreshDatabase.class)
+                .setConstraints(constraints)
+                .setInputData(data)
+                //.setInitialDelay(5, TimeUnit.MINUTES)
+                //.addTag("myTag")
+                .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
+
+        */
+
+        WorkRequest workRequest=new PeriodicWorkRequest.Builder(RefreshDatabase.class,15,TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                if (workInfo.getState()== WorkInfo.State.RUNNING){
+                    System.out.println("Running");
+                } else if (workInfo.getState()== WorkInfo.State.SUCCEEDED) {
+                    System.out.println("succeded");
+                }else if (workInfo.getState()==WorkInfo.State.FAILED){
+                    System.out.println("failed");
+                }
+            }
+        });
+
+        //WorkManager.getInstance(this).cancelWorkById(workRequest.getId());
+
+        //Channing
+
+        /*
+        OneTimeWorkRequest oneTimeWorkRequest=new OneTimeWorkRequest.Builder(RefreshDatabase.class)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+
+         */
+    }
+}
